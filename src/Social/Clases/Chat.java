e5ae700d22029e1f1f3273223a14cc2a151542e7
mancmateo <mancmateo@gmail.com>
2024-06-08 21:06:08 -0600

    agregue los tributos de chat. Faltan los metodos

--- src/Social/Clases/Chat.java
+++ src/Social/Clases/Chat.java
@@ -1,5 +1,41 @@
 package Social.Clases;
 
+import java.util.LinkedHashSet;
+
+import Usuarios.Clases.Usuario;
+
 public class Chat {
+    private Usuario usuarioA;
+    private Usuario usuarioB;
+    private LinkedHashSet <Mensaje> mensajes;
+
+    public Usuario getUsuarioA() {
+        return usuarioA;
+    }
+    public void setUsuarioA(Usuario usuarioA) {
+        this.usuarioA = usuarioA;
+    }
+    public Usuario getUsuarioB() {
+        return usuarioB;
+    }
+    public void setUsuarioB(Usuario usuarioB) {
+        this.usuarioB = usuarioB;
+    }
+    public LinkedHashSet<Mensaje> getMensajes() {
+        return mensajes;
+    }
+    public void setMensajes(LinkedHashSet<Mensaje> mensajes) {
+        this.mensajes = mensajes;
+    }
+
+    //cons
+
+    public Chat(Usuario usuarioA, Usuario usuarioB, LinkedHashSet<Mensaje> mensajes) {
+        this.usuarioA = usuarioA;
+        this.usuarioB = usuarioB;
+        this.mensajes = mensajes;
+    }
+
+    //methods
     
 }

2cf38d09ebb911f0a797c53bff7e41c42e5a490a
GeronimoGM <geronimogonzalezmartino@gmail.com>
2024-06-09 14:49:12 -0300

    Cambié LinkedHashSet en Mensaje por LinkedList y añadí el método enviarMensaje

--- src/Social/Clases/Chat.java
+++ src/Social/Clases/Chat.java
@@ -1,13 +1,13 @@
 package Social.Clases;
 
-import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 import Usuarios.Clases.Usuario;
 
 public class Chat {
     private Usuario usuarioA;
     private Usuario usuarioB;
-    private LinkedHashSet <Mensaje> mensajes;
+    private LinkedList<Mensaje> mensajes;
 
     public Usuario getUsuarioA() {
         return usuarioA;
@@ -21,21 +21,31 @@ public class Chat {
     public void setUsuarioB(Usuario usuarioB) {
         this.usuarioB = usuarioB;
     }
-    public LinkedHashSet<Mensaje> getMensajes() {
+    public LinkedList<Mensaje> getMensajes() {
         return mensajes;
     }
-    public void setMensajes(LinkedHashSet<Mensaje> mensajes) {
+    public void setMensajes(LinkedList<Mensaje> mensajes) {
         this.mensajes = mensajes;
     }
 
     //cons
 
-    public Chat(Usuario usuarioA, Usuario usuarioB, LinkedHashSet<Mensaje> mensajes) {
+    public Chat(Usuario usuarioA, Usuario usuarioB) {
+        this.usuarioA = usuarioA;
+        this.usuarioB = usuarioB;
+        mensajes = new LinkedList<>();
+    }
+
+    public Chat(Usuario usuarioA, Usuario usuarioB, LinkedList<Mensaje> mensajes) {
         this.usuarioA = usuarioA;
         this.usuarioB = usuarioB;
         this.mensajes = mensajes;
     }
 
     //methods
-    
+
+    public void enviarMensaje(String mensaje) {
+        mensajes.add(new Mensaje(mensaje, usuarioA));
+    }
+
 }

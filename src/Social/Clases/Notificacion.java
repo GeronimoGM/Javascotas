0e5ca434807b9b612649a48e5b5f1ac420c135b4
GeronimoGM <geronimogonzalezmartino@gmail.com>
2024-06-09 15:12:11 -0300

    Añadí atributos, constructor y getters y setters en Notificacion

--- src/Social/Clases/Notificacion.java
+++ src/Social/Clases/Notificacion.java
@@ -3,5 +3,16 @@ package Social.Clases;
 import Social.Interfaces.Notificadora;
 
 public class Notificacion<T extends Notificadora<T>> {
-    
+    public T notificacion;
+    // Constructores
+    public Notificacion(T notificacion) {
+        this.notificacion = notificacion;
+    }
+    // Getters y setters
+    public T getNotificacion() {
+        return notificacion;
+    }
+    public void setNotificacion(T notificacion) {
+        this.notificacion = notificacion;
+    }
 }

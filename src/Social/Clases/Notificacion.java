35998e14d4531848c66d51a61f2db1c8028c364d
GeronimoGM <geronimogonzalezmartino@gmail.com>
2024-06-09 08:31:40 -0300

    Corregi la genericidad de la interfaz notificadora y la clase notificacion

--- src/Social/Clases/Notificacion.java
+++ src/Social/Clases/Notificacion.java
@@ -2,6 +2,6 @@ package Social.Clases;
 
 import Social.Interfaces.Notificadora;
 
-public class Notificacion<T extends Notificadora> {
+public class Notificacion<T extends Notificadora<T>> {
     
 }
--- src/Social/Interfaces/Notificadora.java
+++ src/Social/Interfaces/Notificadora.java
@@ -1,5 +1,8 @@
 package Social.Interfaces;
 
-public interface Notificadora {
-    
+import Social.Clases.Notificacion;
+import Usuarios.Clases.Usuario;
+
+public interface Notificadora<T extends Notificadora<T>> {
+    public Notificacion<T> notificar(Usuario usuario);
 }

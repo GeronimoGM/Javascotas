2162af291f6e46b2f5ae7ba8b92f320d395d8977
mancmateo <mancmateo@gmail.com>
2024-06-08 21:09:57 -0600

    agregue los atributos de mensaje. Faltan los metodos

--- src/Social/Clases/Mensaje.java
+++ src/Social/Clases/Mensaje.java
@@ -1,7 +1,42 @@
 package Social.Clases;
 
+import java.time.LocalDate;
+
 import Social.Interfaces.Notificadora;
+import Usuarios.Clases.Usuario;
 
 public class Mensaje implements Notificadora {
+    private LocalDate hora;
+    private String texto;
+    private Usuario usuario;
+    
+    public LocalDate getHora() {
+        return hora;
+    }
+    public void setHora(LocalDate hora) {
+        this.hora = hora;
+    }
+    public String getTexto() {
+        return texto;
+    }
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    //cons
+
+    public Mensaje(LocalDate hora, String texto, Usuario usuario) {
+        this.hora = hora;
+        this.texto = texto;
+        this.usuario = usuario;
+    }
+
+    //methods
     
 }

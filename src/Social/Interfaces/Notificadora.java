6b10f11ac0b9834f3c1d12bb26912ba7ae1e99df
GeronimoGM <geronimogonzalezmartino@gmail.com>
2024-06-09 14:58:47 -0300

    Cambié el tipo de retorno de notificar en Notificadora de Notificacion a Usuario

--- src/Social/Interfaces/Notificadora.java
+++ src/Social/Interfaces/Notificadora.java
@@ -1,8 +1,7 @@
 package Social.Interfaces;
 
-import Social.Clases.Notificacion;
 import Usuarios.Clases.Usuario;
 
 public interface Notificadora<T extends Notificadora<T>> {
-    public Notificacion<T> notificar(Usuario usuario);
+    public Usuario notificar(Usuario usuario);
 }

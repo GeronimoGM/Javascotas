39dbfa621da6141b1bc1cc56d9b24c54eb17d824
mancmateo <mancmateo@gmail.com>
2024-06-09 16:15:34 -0600

    metodos publicar, eliminar publicacion, comentar y eliminar comentario aniadidos a usuario owo

--- src/Usuarios/Clases/Usuario.java
+++ src/Usuarios/Clases/Usuario.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
 import Social.Clases.Chat;
+import Social.Clases.Comentario;
+import Social.Clases.Mensaje;
 import Social.Clases.Notificacion;
 import Social.Clases.Publicacion;
 import Usuarios.Clases.Mascotas.Abstracta.Mascota;
@@ -20,10 +23,12 @@ public class Usuario {
     private int edad;
     private File foto;
     private char sexo;
+    private LinkedHashSet <Publicacion> publicaciones;
     private ArrayList<Mascota> mascotas;
     private LinkedList<Chat> chats;
     private LinkedHashSet<Notificacion<?>> notificaciones;
     private HashSet<Usuario> bloqueados;
+
     // Constructores
     public Usuario(String username, String nombre, String contrasena, LocalDate fechaDeNacimiento, int edad, File foto,
             char sexo) {
@@ -39,49 +44,64 @@ public class Usuario {
         notificaciones = new LinkedHashSet<>();
         bloqueados = new HashSet<>();
     }
+
     // Getters y setters
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getContrasena() {
         return contrasena;
     }
+
     public void setContrasena(String contrasena) {
         this.contrasena = contrasena;
     }
+
     public LocalDate getFechaDeNacimiento() {
         return fechaDeNacimiento;
     }
+
     public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
         this.fechaDeNacimiento = fechaDeNacimiento;
     }
+
     public int getEdad() {
         return edad;
     }
+
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
     public File getFoto() {
         return foto;
     }
+
     public void setFoto(File foto) {
         this.foto = foto;
     }
+
     public char getSexo() {
         return sexo;
     }
+
     public void setSexo(char sexo) {
         this.sexo = sexo;
     }
+
     // Métodos
     @Override
     public int hashCode() {
@@ -90,6 +110,7 @@ public class Usuario {
         result = prime * result + ((username == null) ? 0 : username.hashCode());
         return result;
     }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -106,4 +127,51 @@ public class Usuario {
             return false;
         return true;
     }
+
+    public LinkedHashSet <Publicacion> publicar(Publicacion publicacion){
+
+        publicaciones.add(publicacion);
+        return publicaciones;
+    }
+
+    public boolean eliminarPunlicacion(Publicacion publicacion){
+        boolean eliminado = false;
+        Iterator <Publicacion> iterador = publicaciones.iterator();
+        
+        while (iterador.hasNext()) {
+            if(iterador.next().equals(publicacion)){
+                iterador.remove();
+                eliminado = true;
+            }
+        }
+        return eliminado;
+    }
+
+    public Publicacion comentarPublicacion(Publicacion publicacion, Comentario comentario){
+        Iterator <Publicacion> iterador = publicaciones.iterator();
+        Publicacion aux = iterador.next();
+        while (iterador.hasNext()) {
+            if (aux.equals(publicacion)){
+                aux.anadirComentario(comentario);
+            }else{
+                aux = iterador.next();
+            }
+        }
+        return aux;
+    }
+    
+    public Publicacion eliminarComentario(Publicacion publicacion, Comentario comentario){
+        Iterator <Publicacion> iterador = publicaciones.iterator();
+        Publicacion aux = iterador.next();
+        while (iterador.hasNext()) {
+            if (aux.equals(publicacion)){
+                iterador.remove();
+            }else{
+                aux = iterador.next();
+            }
+        }
+        return aux;
+    }
+
+    
 }

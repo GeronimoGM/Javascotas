e77635fccf4e62fdcab23fbf81b3f49273cf04ed
mancmateo <mancmateo@gmail.com>
2024-06-08 21:15:09 -0600

    agregue los atributos de comentario. Faltan los metodos

--- src/Social/Clases/Comentario.java
+++ src/Social/Clases/Comentario.java
@@ -1,7 +1,59 @@
 package Social.Clases;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+
 import Social.Interfaces.Notificadora;
+import Usuarios.Clases.Usuario;
 
 public class Comentario implements Notificadora {
+    private Publicacion publicacion;
+    private Usuario usuario;
+    private String texto;
+    private LocalDate hora;
+    private HashSet <Usuario> likes;
+
+    public Publicacion getPublicacion() {
+        return publicacion;
+    }
+    public void setPublicacion(Publicacion publicacion) {
+        this.publicacion = publicacion;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    public String getTexto() {
+        return texto;
+    }
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    public LocalDate getHora() {
+        return hora;
+    }
+    public void setHora(LocalDate hora) {
+        this.hora = hora;
+    }
+    public HashSet<Usuario> getLikes() {
+        return likes;
+    }
+    public void setLikes(HashSet<Usuario> likes) {
+        this.likes = likes;
+    }
+
+    //cons
+
+    public Comentario(Publicacion publicacion, Usuario usuario, String texto, LocalDate hora, HashSet<Usuario> likes) {
+        this.publicacion = publicacion;
+        this.usuario = usuario;
+        this.texto = texto;
+        this.hora = hora;
+        this.likes = likes;
+    }
+
+    //methods
     
 }
--- src/Social/Clases/Like.java
+++ src/Social/Clases/Like.java
@@ -1,7 +1,26 @@
 package Social.Clases;
 
 import Social.Interfaces.Notificadora;
+import Usuarios.Clases.Usuario;
 
 public class Like implements Notificadora {
+    private Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    // cons
+
+    public Like(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    //methods
+
     
 }

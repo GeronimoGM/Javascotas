461ab11333cea48189ae22ff215acedc7272f85d
GeronimoGM <geronimogonzalezmartino@gmail.com>
2024-06-12 16:04:33 -0300

    Añadí función equals y hashCode a Mensaje

--- src/Backend/Social/Clases/Mensaje.java
+++ src/Backend/Social/Clases/Mensaje.java
@@ -31,4 +31,28 @@ public class Mensaje implements Notificadora<Mensaje> {
         usuario.anadirNotificacion(new Notificacion<Mensaje>(this));
         return usuario;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((hora == null) ? 0 : hora.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Mensaje other = (Mensaje) obj;
+        if (hora == null) {
+            if (other.hora != null)
+                return false;
+        } else if (!hora.equals(other.hora))
+            return false;
+        return true;
+    }
 }

4448a2791dee0b6cca5bfac853676a1e89dbcd4b
GeronimoGM <geronimogonzalezmartino@gmail.com>
2024-06-17 08:22:41 -0300

    Hice que antes que se inicie el programa se carguen todos los usuarios

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
+import Backend.Gestor.Clases.Gestor;
 import Frontend.JMainFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Gestor.cargar();
         new JMainFrame("Javascotas");
     }
 }
